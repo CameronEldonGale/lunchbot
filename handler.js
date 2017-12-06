b88cd5e5912f796f8f0260d86cbb8edce670c4a8
Cameron Gale <cameroneldongale@gmail.com>
2017-12-06 06:27:40 -0700

    save work

--- handler.js
+++ handler.js
@@ -2,10 +2,12 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+let result = 'um something went wrong'
+
 
 module.exports.post = (event, context, callback) => {
   const token = process.env.token
-  const message = `Go Serverless v1.0! Your function executed successfully! value: ${ token }`;
+  const message = `Go Serverless v1.0! Your function executed successfully! result: ${ result }`;
   const response = {
     statusCode: 200,
     body: JSON.stringify({
@@ -23,6 +25,21 @@ module.exports.post = (event, context, callback) => {
 
 function scraper() {
   return new Promise(function(resolve, reject) {
+    getWeeklyMenu()
+      .then((weeklyMenuLink) => {
+        console.log('weekly menu', weeklyMenuLink);
+        getLunch(weeklyMenuLink)
+
+      })
+      .catch((err) => {
+        return reject(err);
+      })
+  });
+
+}
+
+function getWeeklyMenu() {
+  return new Promise(function(resolve, reject) {
     var rightNow = new Date();
     var yyyymmdd = rightNow.toISOString().slice(0,10);
 
@@ -30,7 +47,32 @@ function scraper() {
 
     axios.get(url)
       .then((response) => {
-        console.log(response);
+        const $ = cheerio.load(response.data);
+        const menuLink = $('.hidden-small').prop('href');
+        return resolve(menuLink);
+      })
+      .catch((err) => {
+        return reject(err);
       })
   });
 }
+
+function getLunch(weeklyMenuLink) {
+  console.log('fetching today\'s lunch...');
+  return new Promise(function(resolve, reject) {
+    axios.get(weeklyMenuLink)
+      .then((response) => {
+        const $ = cheerio.load(response.data);
+        const table = $('.container').children('.main');
+        const chefsTable = table.children();
+        const cells = $('.cell_menu_item').toArray();
+        const text = cells.map(function(i, el) {
+            // this === el
+            return $(this).last().text();
+          })
+        console.log(text);
+  });
+})
+}
+
+scraper();
